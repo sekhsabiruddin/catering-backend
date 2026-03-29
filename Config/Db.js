610303a50e8529963a3d3d4e23afbfbae030be48
sabir-kas <sabiruddin@kascommerce.com>
2026-03-29 22:10:46 +0530

    ch

--- Config/Db.js
+++ Config/Db.js
@@ -2,16 +2,17 @@
 require('dotenv').config();
 const mysql = require("mysql2");
 
+const pool = process.env.DATABASE_URL
+  ? mysql.createPool(process.env.DATABASE_URL)
+  : mysql.createPool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+    });
 
-// Create a connection pool
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
 module.exports = pool.promise();
